02053ffcb0c2782c1698bb516f4b004a767a5f3d
siam114 <smsiamislam598@gmail.com>
2024-09-23 13:32:04 +0600

    added blog section connection the html page

--- js/index.js
+++ js/index.js
@@ -74,13 +74,12 @@ historyTab.addEventListener('click', function () {
     donationTab.classList.add( 'border','border-[#11111146]',);
 
     document.getElementById('donation-section').classList.add('hidden');
-    document.getElementById('expense-form').classList.remove('hidden');
-   
+    document.getElementById('expense-form').classList.remove('hidden');  
+    
 });
      
 donationTab.addEventListener('click', function () {
     switchTabs(donationTab, historyTab);
-   
 
     document.getElementById('donation-section').classList.remove('hidden');
     document.getElementById('expenses-section').classList.add('hidden');
