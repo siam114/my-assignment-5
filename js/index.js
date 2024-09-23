56cee18088a1822a8b193f6081b0cd1c6968962f
siam114 <smsiamislam598@gmail.com>
2024-09-23 23:34:13 +0600

    added history section

--- js/index.js
+++ js/index.js
@@ -42,16 +42,25 @@ document.getElementById('donate-noakhali').addEventListener('click',function(){
         document.getElementById('donate-modal').classList.add('hidden');
     };
     
+    // history section
+    const historyItem = document.createElement('div');
+    historyItem.className = "sm:mx-32 mx-5 p-5 border rounded-lg border-indigo-500";
+    historyItem.innerHTML = `
+        <p>${inputNoakhali.toFixed(2)} Taka Donate for Flood at Noakhali, Bangladesh</p>
+        <p class="bg-gray-100 p-2 rounded-md">Date : ${new Date()}</p>
+    `
+    const historyContainer = document.getElementById('history-section');
+    historyContainer.insertBefore(historyItem, historyContainer.firstChild);
 })
 
 document.getElementById('donate-feni').addEventListener('click', function(){
 
     const accountBalance = getFieldValueById('account-balance')
-    const inputNoakhali = getInputValueById('input-feni')
+    const inputFeni = getInputValueById('input-feni')
     const donateBalance = getFieldValueById('donate-balance-feni')
         
-    const newAccountBalance = accountBalance - inputNoakhali ;
-    const newDonateBalance = inputNoakhali + donateBalance;
+    const newAccountBalance = accountBalance - inputFeni ;
+    const newDonateBalance = inputFeni + donateBalance;
     
     document.getElementById('donate-balance-feni').innerText = newDonateBalance;
     document.getElementById('account-balance').innerText = newAccountBalance;
@@ -63,16 +72,27 @@ document.getElementById('donate-feni').addEventListener('click', function(){
     document.getElementById('close-modal').onclick = function() {
         document.getElementById('donate-modal').classList.add('hidden');
     };
+
+
+     // history section
+     const historyItem = document.createElement('div');
+     historyItem.className = "sm:mx-32 mx-5 p-5 border rounded-lg border-indigo-500";
+     historyItem.innerHTML = `
+         <p>${inputFeni.toFixed(2)} Taka  Donate for Flood at Feni, Bangladesh</p>
+         <p class="bg-gray-100 p-2 rounded-md">Date : ${new Date()}</p>
+     `
+     const historyContainer = document.getElementById('history-section');
+     historyContainer.insertBefore(historyItem, historyContainer.firstChild);
 })
 
 document.getElementById('donate-qouta').addEventListener('click', function(){
 
     const accountBalance = getFieldValueById('account-balance')
-    const inputNoakhali = getInputValueById('input-qouta')
+    const inputQouta = getInputValueById('input-qouta')
     const donateBalance = getFieldValueById('donate-balance-qouta')
         
-    const newAccountBalance = accountBalance - inputNoakhali ;
-    const newDonateBalance = inputNoakhali + donateBalance;
+    const newAccountBalance = accountBalance - inputQouta ;
+    const newDonateBalance = inputQouta + donateBalance;
     
     document.getElementById('donate-balance-qouta').innerText = newDonateBalance;
     document.getElementById('account-balance').innerText = newAccountBalance;
@@ -84,6 +104,16 @@ document.getElementById('donate-qouta').addEventListener('click', function(){
     document.getElementById('close-modal').onclick = function() {
         document.getElementById('donate-modal').classList.add('hidden')
     };
+
+     // history section
+     const historyItem = document.createElement('div');
+     historyItem.className = "sm:mx-32 mx-5 p-5 border rounded-lg border-indigo-500";
+     historyItem.innerHTML = `
+         <p>${inputQouta.toFixed(2)} Taka Aid for Injured in the Quota Movement</p>
+         <p class="bg-gray-100 p-2 rounded-md">Date : ${new Date()}</p>
+     `
+     const historyContainer = document.getElementById('history-section');
+     historyContainer.insertBefore(historyItem, historyContainer.firstChild);
 })
 
 
@@ -96,8 +126,7 @@ historyTab.addEventListener('click', function () {
     donationTab.classList.add( 'border','border-[#11111146]',);
 
     document.getElementById('donation-section').classList.add('hidden');
-    document.getElementById('expense-form').classList.remove('hidden');
-    document.getElementById('footer').classList.add('hidden'); 
+    document.getElementById('history-section').classList.remove('hidden');
     
 });
      
@@ -108,7 +137,7 @@ donationTab.addEventListener('click', function () {
     donationTab.classList.remove( 'border','border-[#11111146]',);
 
     document.getElementById('donation-section').classList.remove('hidden');
-    document.getElementById('expenses-section').classList.add('hidden');
+    document.getElementById('history-section').classList.add('hidden');
    
 });
 
